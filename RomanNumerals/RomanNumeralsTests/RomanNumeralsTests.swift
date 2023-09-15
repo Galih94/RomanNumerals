bd2b36ef4f79f95e7275dd6646dce997d9308caa
galih <samudragalih94@gmail.com>
2023-09-15 15:05:38 +0700

    move(SymbolTests): move Symbol tests to SymbolTests

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -9,24 +9,4 @@ import XCTest
 import RomanNumerals
 
 final class RomanNumeralsTests: XCTestCase {
-    func test_translateSymbols() {
-        expect(from: [.I, .V, .X, .L, .C, .D, .M], expectAnswers: ["I", "V", "X", "L", "C", "D", "M"])
-    }
-    
-    func test_convertNominalTextGreekToNumber() {
-        expect(from: [.I, .V, .X, .L, .C, .D, .M], expectAnswers: [1, 5, 10, 50, 100, 500, 1000])
-    }
-    
-    // MARK: Helper
-    private func expect(from symbols: [Symbol], expectAnswers: [String], file: StaticString = #filePath, line: UInt = #line ) {
-        symbols.enumerated().forEach { index, symbol in
-            XCTAssertEqual(symbol.rawValue, expectAnswers[index], file: file, line: line)
-        }
-    }
-    
-    private func expect(from symbols: [Symbol], expectAnswers: [Int], file: StaticString = #filePath, line: UInt = #line ) {
-        symbols.enumerated().forEach { index, symbol in
-            XCTAssertEqual(symbol.convertNominalTextGreekToNumber(), expectAnswers[index], file: file, line: line)
-        }
-    }
 }
