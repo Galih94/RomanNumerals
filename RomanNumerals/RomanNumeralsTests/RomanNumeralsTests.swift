93d4470e553245218f0772c9e5dbbcc3f460d63d
galih <samudragalih94@gmail.com>
2023-09-15 20:02:39 +0700

    feat(convertRomanTextToNumber): update case `convertRomanTextToNumber` make throws error on wrong text

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -11,13 +11,24 @@ import RomanNumerals
 public final class RomanNumerals {
     private init () {}
     
-    public static func convertTextGreekToNumber(_ text: String) -> Int {
-        return 1
+    public static func convertRomanTextToNumber(_ text: String) throws -> Int {
+        let array = text.map { String(describing: $0) }
+        var result = 0
+        for i in 0..<array.count {
+            guard let symbolValue = Symbol(rawValue: array[i]) else {
+                throw NSError(domain: "Found error on converting Roman text ", code: 0)
+            }
+            let currentTextValue = symbolValue.convertNominalTextGreekToNumber()
+            result += currentTextValue
+        }
+        return result
     }
 }
 
 final class RomanNumeralsTests: XCTestCase {
     func test_convertTextGreekToNumber() {
-        XCTAssertEqual(RomanNumerals.convertTextGreekToNumber("I"), 1)
+        XCTAssertEqual(try? RomanNumerals.convertRomanTextToNumber("I"), 1)
+        XCTAssertEqual(try? RomanNumerals.convertRomanTextToNumber("II"), 2)
+        XCTAssertNoThrow(try? RomanNumerals.convertRomanTextToNumber("A"))
     }
 }
