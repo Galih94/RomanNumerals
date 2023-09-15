1de19ff633f566dd90ee3b053840ef84842a6e21
galih <samudragalih94@gmail.com>
2023-09-15 14:59:17 +0700

    feat(convertNominalTextGreekToNumber): create convertNominalTextGreekToNumber

--- RomanNumerals/RomanNumerals/Symbol.swift
+++ RomanNumerals/RomanNumerals/Symbol.swift
@@ -13,4 +13,23 @@ public enum Symbol: String {
     case C
     case D
     case M
+    
+    public func convertNominalTextGreekToNumber() -> Int {
+        switch self {
+        case .I:
+            return 1
+        case .V:
+            return 5
+        case .X:
+            return 10
+        case .L:
+            return 50
+        case .C:
+            return 100
+        case .D:
+            return 500
+        case .M:
+            return 1000
+        }
+    }
 }
--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -13,6 +13,16 @@ final class RomanNumeralsTests: XCTestCase {
         expect(from: [.I, .V, .X, .L, .C, .D, .M], expectAnswers: ["I", "V", "X", "L", "C", "D", "M"])
     }
     
+    func test_convertNominalTextGreekToNumber() {
+        XCTAssertEqual(Symbol.I.convertNominalTextGreekToNumber(), 1)
+        XCTAssertEqual(Symbol.V.convertNominalTextGreekToNumber(), 5)
+        XCTAssertEqual(Symbol.X.convertNominalTextGreekToNumber(), 10)
+        XCTAssertEqual(Symbol.L.convertNominalTextGreekToNumber(), 50)
+        XCTAssertEqual(Symbol.C.convertNominalTextGreekToNumber(), 100)
+        XCTAssertEqual(Symbol.D.convertNominalTextGreekToNumber(), 500)
+        XCTAssertEqual(Symbol.M.convertNominalTextGreekToNumber(), 1000)
+    }
+    
     // MARK: Helper
     private func expect(from symbols: [Symbol], expectAnswers: [String], file: StaticString = #filePath, line: UInt = #line ) {
         symbols.enumerated().forEach { index, symbol in
