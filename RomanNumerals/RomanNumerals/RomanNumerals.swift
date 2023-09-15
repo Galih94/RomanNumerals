0194c32aff971dfc022570f3deeefcea4d3245c3
galih <samudragalih94@gmail.com>
2023-09-15 22:01:53 +0700

    refactor(RomanNumerals): simplify code

--- RomanNumerals/RomanNumerals/RomanNumerals.swift
+++ RomanNumerals/RomanNumerals/RomanNumerals.swift
@@ -19,29 +19,17 @@ public enum RomanNumerals {
     static let ANY_ERROR_CODE = 0
     
     private static func calculate(from accumulation: Int, with number: Int, previousNumber: Int) -> Int {
-        if isAccumulationZero(accumulation) {
+        if accumulation == ZERO_NUMBER {
             return number
         } else if previousNumber < number {
             return (accumulation - previousNumber) + (number - previousNumber)
         } else {
-            return addAccumulationAndNewNumber(from: accumulation, with: number)
+            return accumulation + number
         }
     }
     
-    private static func isFirstIndex(_ index: Int) -> Bool {
-        return index == ZERO_INDEX
-    }
-    
-    private static func previousIndex(_ index: Int) -> Int? {
-        return isFirstIndex(index) ? nil : index - ONE_INDEX
-    }
-    
-    private static func isAccumulationZero(_ accumulation: Int) -> Bool {
-        return accumulation == ZERO_NUMBER
-    }
-    
-    private static func addAccumulationAndNewNumber(from accumulation: Int, with number: Int) -> Int {
-        return accumulation + number
+    private static func getPreviousIndex(_ index: Int) -> Int? {
+        return index == ZERO_INDEX ? nil : index - ONE_INDEX
     }
     
     private static func getPreviousIndexNumber(index: Int?, from array: [String]) -> Int {
@@ -56,7 +44,7 @@ public enum RomanNumerals {
         let array = text.map { String(describing: $0) }
         var result = DEFAULT_NUMBER
         for i in ZERO_INDEX..<array.count {
-            let previousNumber = getPreviousIndexNumber(index: previousIndex(i), from: array)
+            let previousNumber = getPreviousIndexNumber(index: getPreviousIndex(i), from: array)
             guard let symbolValue = Symbol(rawValue: array[i]) else {
                 throw NSError(domain: "Found error on converting Roman text ", code: ANY_ERROR_CODE)
             }
