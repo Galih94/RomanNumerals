c08cadec8ec20cacb063b55ddc572daae246feee
galih <samudragalih94@gmail.com>
2023-09-15 14:35:43 +0700

    feat(Symbol): create Symbol enums

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -7,6 +7,25 @@
 
 import XCTest
 
+public enum Symbol: String {
+    case I
+    case V
+    case X
+    case L
+    case C
+    case D
+    case M
+}
+
 final class RomanNumeralsTests: XCTestCase {
 
+    func test_translateSymbols() {
+        XCTAssertEqual(Symbol.I.rawValue, "I")
+        XCTAssertEqual(Symbol.V.rawValue, "V")
+        XCTAssertEqual(Symbol.X.rawValue, "X")
+        XCTAssertEqual(Symbol.L.rawValue, "L")
+        XCTAssertEqual(Symbol.C.rawValue, "C")
+        XCTAssertEqual(Symbol.D.rawValue, "D")
+        XCTAssertEqual(Symbol.M.rawValue, "M")
+    }
 }
