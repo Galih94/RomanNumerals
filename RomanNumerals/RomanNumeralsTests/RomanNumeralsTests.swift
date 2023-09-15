832a768b3850ae1ba781210016797ab723b93d8f
galih <samudragalih94@gmail.com>
2023-09-15 22:29:37 +0700

    refactor(RomanNumeralsTests): refactor test assert XCTAssertThrowsError

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -10,22 +10,26 @@ import RomanNumerals
 
 final class RomanNumeralsTests: XCTestCase {
     func test_convertTextGreekToNumber() {
-        expect(from: ["I", "II", "IV", "IX", "X", "XI", "XXX", "LI", "LIV", "CIX", "CX", "MCMXCIV"],
-               expectAnswers: [1, 2, 4, 9, 10, 11, 30, 51, 54, 109, 110, 1994])
-        expectErrors(from: ["A", "GALIH", "G", "1"])
+        do {
+            try expect(from: ["I", "II", "IV", "IX", "X", "XI", "XXX", "LI", "LIV", "CIX", "CX", "MCMXCIV"],
+                       expectAnswers: [1, 2, 4, 9, 10, 11, 30, 51, 54, 109, 110, 1994])
+            try expectErrors(from: ["A", "GALIH", "G", "1"])
+        } catch {
+            XCTFail("Failed to run tests")
+        }
     }
     
     // MARK: Helper
     
-    private func expect(from texts: [String], expectAnswers: [Int], file: StaticString = #filePath, line: UInt = #line ) {
-        texts.enumerated().forEach { index, text in
-            XCTAssertEqual(try? RomanNumerals.convertRomanTextToNumber(text), expectAnswers[index], file: file, line: line)
+    private func expect(from texts: [String], expectAnswers: [Int], file: StaticString = #filePath, line: UInt = #line ) throws {
+        try texts.enumerated().forEach { index, text in
+            XCTAssertEqual(try RomanNumerals.convertRomanTextToNumber(text), expectAnswers[index], file: file, line: line)
         }
     }
     
-    private func expectErrors(from texts: [String], file: StaticString = #filePath, line: UInt = #line ) {
-        texts.forEach { text in
-            XCTAssertNoThrow(try? RomanNumerals.convertRomanTextToNumber(text), file: file, line: line)
+    private func expectErrors(from texts: [String], file: StaticString = #filePath, line: UInt = #line ) throws {
+        try texts.forEach { text in
+            XCTAssertThrowsError(try RomanNumerals.convertRomanTextToNumber(text), file: file, line: line)
         }
     }
 }
