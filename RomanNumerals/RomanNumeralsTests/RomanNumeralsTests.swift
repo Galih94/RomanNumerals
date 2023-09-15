cbf9c9554f6de36629ce6c29696388393d897429
galih <samudragalih94@gmail.com>
2023-09-15 19:49:25 +0700

    feat(convertTextGreekToNumber): create `convertTextGreekToNumber`

--- RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -8,5 +8,16 @@
 import XCTest
 import RomanNumerals
 
+public final class RomanNumerals {
+    private init () {}
+    
+    public static func convertTextGreekToNumber(_ text: String) -> Int {
+        return 1
+    }
+}
+
 final class RomanNumeralsTests: XCTestCase {
+    func test_convertTextGreekToNumber() {
+        XCTAssertEqual(RomanNumerals.convertTextGreekToNumber("I"), 1)
+    }
 }
