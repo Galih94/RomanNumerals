924def6068d548347944ba8077ee795a6f5d1be3
galih <samudragalih94@gmail.com>
2023-09-18 11:16:59 +0700

    refactor(LatinNumbers): refactor `convertRomanNumerals` add test to convert just single symbols

--- RomanNumerals/RomanNumeralsTests/LatinNumbersTests.swift
+++ RomanNumerals/RomanNumeralsTests/LatinNumbersTests.swift
@@ -9,13 +9,26 @@ import XCTest
 import RomanNumerals
 
 public enum LatinNumbers {
+    
     public static func convertRomanNumerals(from number: Int) -> String {
+        guard NumberSymbol.convert(number) != "" else {
+            return ""
+        }
         return NumberSymbol.convert(number)
     }
 }
 
 final class LatinNumbersTests: XCTestCase {
     func test_convertNumber() {
-        XCTAssertEqual(LatinNumbers.convertRomanNumerals(from: 1), "I")
+        expect(from: [1, 5, 10, 50, 100, 500, 1000, 0],
+               expectedSymbols: ["I", "V", "X", "L", "C", "D", "M", ""])
     }
+    
+    // MARK: Helper
+    private func expect(from numbers: [Int], expectedSymbols: [String], file: StaticString = #filePath, line: UInt = #line ) {
+        for i in 0..<numbers.count {
+            XCTAssertEqual(LatinNumbers.convertRomanNumerals(from: numbers[i]), expectedSymbols[i], file: file, line: line)
+        }
+    }
+
 }
