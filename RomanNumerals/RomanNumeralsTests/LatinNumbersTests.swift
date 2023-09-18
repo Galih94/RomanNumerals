1c74bd072964bd42c55aa74f02c4800dcc234c1e
galih <samudragalih94@gmail.com>
2023-09-18 13:20:33 +0700

    feat(LatinNumbers): add test case 1 - 5

--- RomanNumerals/RomanNumeralsTests/LatinNumbersTests.swift
+++ RomanNumerals/RomanNumeralsTests/LatinNumbersTests.swift
@@ -9,11 +9,23 @@ import XCTest
 import RomanNumerals
 
 public enum LatinNumbers {
+    private static func calculate(from number: Int) -> String {
+        if number == 0 {
+            return ""
+        } else if number < 5 {
+            if number == 5 - 1 {
+                return "IV"
+            } else {
+                return "I\(LatinNumbers.calculate(from: number - 1))"
+            }
+        } else {
+            return ""
+        }
+    }
     
     public static func convertRomanNumerals(from number: Int) -> String {
         guard NumberSymbol.convert(number) != "" else {
-            
-            return ""
+            return calculate(from: number)
         }
         return NumberSymbol.convert(number)
     }
@@ -23,6 +35,11 @@ final class LatinNumbersTests: XCTestCase {
     func test_convertNumber_singleSymbol() {
         expect(from: [1, 5, 10, 50, 100, 500, 1000, 0],
                expectedSymbols: ["I", "V", "X", "L", "C", "D", "M", ""])
+    }
+    
+    func test_convertNumber_underFive() {
+        expect(from: [1,2,3,4],
+               expectedSymbols: ["I", "II", "III", "IV"])
     }
     
     // MARK: Helper
