80e6eb162ff96929c7339bdafba05749344d0914
galih <samudragalih94@gmail.com>
2023-09-18 15:41:47 +0700

    refactor(LatinNumbers): refactor `LatinNumbers`

--- RomanNumerals/RomanNumerals/LatinNumbers.swift
+++ RomanNumerals/RomanNumerals/LatinNumbers.swift
@@ -6,56 +6,49 @@
 //
 
 public enum LatinNumbers {
-    private static func calculate(from number: Int) -> String {
+    public static func convertRomanNumerals(from number: Int) -> String {
         if number == 0 {
             return ""
         } else if (1...4).contains(number) {
             if number == 5 - 1 {
                 return "IV"
             } else {
-                return "I\(LatinNumbers.calculate(from: number - 1))"
+                return "I\(LatinNumbers.convertRomanNumerals(from: number - 1))"
             }
         } else if (5...9).contains(number) {
             if number == 10 - 1 {
                 return "IX"
             } else {
-                return "V\(LatinNumbers.calculate(from: number - 5))"
+                return "V\(LatinNumbers.convertRomanNumerals(from: number - 5))"
             }
         } else if (10...49).contains(number) {
             if (40...49).contains(number) {
-                return "XL\(LatinNumbers.calculate(from: number - 40))"
+                return "XL\(LatinNumbers.convertRomanNumerals(from: number - 40))"
             } else {
-                return "X\(LatinNumbers.calculate(from: number - 10))"
+                return "X\(LatinNumbers.convertRomanNumerals(from: number - 10))"
             }
         } else if (50...99).contains(number) {
             if (90...99).contains(number) {
-                return "XC\(LatinNumbers.calculate(from: number - 90))"
+                return "XC\(LatinNumbers.convertRomanNumerals(from: number - 90))"
             } else {
-                return "L\(LatinNumbers.calculate(from: number - 50))"
+                return "L\(LatinNumbers.convertRomanNumerals(from: number - 50))"
             }
         } else if (100...499).contains(number) {
             if (400...499).contains(number) {
-                return "CD\(LatinNumbers.calculate(from: number - 400))"
+                return "CD\(LatinNumbers.convertRomanNumerals(from: number - 400))"
             } else {
-                return "C\(LatinNumbers.calculate(from: number - 100))"
+                return "C\(LatinNumbers.convertRomanNumerals(from: number - 100))"
             }
         } else if (500...999).contains(number) {
             if (900...999).contains(number) {
-                return "CM\(LatinNumbers.calculate(from: number - 900))"
+                return "CM\(LatinNumbers.convertRomanNumerals(from: number - 900))"
             } else {
-                return "D\(LatinNumbers.calculate(from: number - 500))"
+                return "D\(LatinNumbers.convertRomanNumerals(from: number - 500))"
             }
         } else if (1000...4999).contains(number) {
-            return "M\(LatinNumbers.calculate(from: number - 1000))"
+            return "M\(LatinNumbers.convertRomanNumerals(from: number - 1000))"
         } else {
             return ""
         }
-    }
-    
-    public static func convertRomanNumerals(from number: Int) -> String {
-        guard NumberSymbol.convert(number) != "" else {
-            return calculate(from: number)
-        }
-        return NumberSymbol.convert(number)
     }
 }
