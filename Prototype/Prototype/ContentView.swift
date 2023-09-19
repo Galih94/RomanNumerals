60836e901452838fee0670adb5323fca9c7f2692
galih <samudragalih94@gmail.com>
2023-09-19 12:31:55 +0700

    create(Prototype): create `Prototype` UI

---
+++ Prototype/Prototype/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Galih Samudra on 19/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Galih Samudra on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
