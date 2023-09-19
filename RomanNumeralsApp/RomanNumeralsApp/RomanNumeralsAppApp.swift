88dd865e6a22a198ed29b2c58ef5148ae66434c0
galih <samudragalih94@gmail.com>
2023-09-19 10:14:56 +0700

    feat(RomanNumeralsApp)

---
+++ RomanNumeralsApp/RomanNumeralsApp/RomanNumeralsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RomanNumeralsAppApp.swift
+//  RomanNumeralsApp
+//
+//  Created by Galih Samudra on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RomanNumeralsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RomanNumeralsApp/RomanNumeralsAppUITests/RomanNumeralsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RomanNumeralsAppUITestsLaunchTests.swift
+//  RomanNumeralsAppUITests
+//
+//  Created by Galih Samudra on 19/09/23.
+//
+
+import XCTest
+
+final class RomanNumeralsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
